02617411c8691e32127aea2c4cc182da6c60c8c3
szgorski <101061451+szgorski@users.noreply.github.com>
2023-10-28 19:31:11 +0200

    plotting finished
    improved readability, points divided to separate classes and sets

--- figure.py
+++ figure.py
@@ -12,7 +12,7 @@ def get_figure(data, layers):
     print(placements_x)
 
     max_layer = max(layers)
-    placements_y = [0.15 + 0.8 * y / (max_layer - 1) for y in range(max_layer)]
+    placements_y = [0.1 + 0.8 * y / (max_layer - 1) for y in range(max_layer)]
     print(placements_y)
 
     node_x = []
@@ -27,14 +27,22 @@ def get_figure(data, layers):
     edge_y = []
     for i in range(layer_count - 1):
         for start in range(index, index + layers[i]):
+            pos_index = 0
+            if layers[i] == 1 or layers[i + 1] == 1:
+                positions = [0.5] * layers[i + 1]
+            else:
+                positions = [0.2 + 0.6 * j / (layers[i + 1] - 1) for j in range(layers[i + 1])]
+
             for end in range(index + layers[i], index + layers[i] + layers[i + 1]):
                 edge_x.append(node_x[start])
-                edge_x.append((2 * node_x[start] + node_x[end]) / 3)
+                edge_x.append((1 - positions[pos_index]) * node_x[start] + positions[pos_index] * node_x[end])
                 edge_x.append(node_x[end])
 
                 edge_y.append(node_y[start])
-                edge_y.append((2 * node_y[start] + node_y[end]) / 3)
+                edge_y.append((1 - positions[pos_index]) * node_y[start] + positions[pos_index] * node_y[end])
                 edge_y.append(node_y[end])
+
+                pos_index += 1
 
         index += layers[i]
 
@@ -47,7 +55,7 @@ def get_figure(data, layers):
     edge_trace = [go.Scatter(
             x=edge_x[3 * i:3 * i + 3], y=edge_y[3 * i:3 * i + 3],
             mode='lines+text',
-            text=['', '<br>{:4.2f}<br>(Δ{:+4.2f})'.format(edge_width[i], edge_change[i]), ''],
+            text=['', '<br>{:5.3f}<br>(Δ{:+5.3f})'.format(edge_width[i], edge_change[i]), ''],
             hoverinfo='none',
             textposition='bottom center',
             line=dict(width=abs(edge_width[i]), color='red' if edge_width[i] < 0 else 'green'))
@@ -57,30 +65,25 @@ def get_figure(data, layers):
         x=[node_x[i]], y=[node_y[i]],
         mode='markers+text',
         hoverinfo='none',
-        text='bias:<br>{:4.2f} (Δ{:+4.2f})'.format(node_value[i], node_change[i])
+        text='bias:<br>{:5.3f}<br>(Δ{:+5.3f})'.format(node_value[i], node_change[i])
         if i >= layers[0] else '<br>IN {}'.format(i + 1),
         textposition='bottom center',
         marker=dict(
             color='red' if node_value[i] < 0 else 'green' if node_value[i] > 0 else 'azure',
             size=15,
-            line_width=1)
-    ) for i in range(len(node_change))]
+            line_width=1))
+        for i in range(len(node_change))]
 
     edge_trace.extend(node_trace)
     fig = go.Figure(data=edge_trace,
                     layout=go.Layout(
-                        title='Bias and weights distribution against iterations',
+                        title='Bias and weights distribution against iterations (choose the iteration above)',
                         titlefont_size=16,
                         showlegend=False,
                         hovermode='closest',
                         margin=dict(b=20, l=5, r=5, t=40),
-                        annotations=[dict(
-                            text="S. Górski, P. Drabarek",
-                            showarrow=False,
-                            xref="paper", yref="paper",
-                            x=0.005, y=-0.002)],
                         xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
-                    )
-    # fig.show()
+                        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)
+                    ))
+
     return fig
